878da3309222267cdc5ad4246427493e199c9dea
Shubham Singh <shubhamkumar2452004@gmail.com>
2026-06-30 11:28:46 +0530

    update variable

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,7 +1,11 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 const app = initializeApp({
   credential: cert(serviceAccount),
