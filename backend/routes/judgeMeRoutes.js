5a471fb84fb80a2c7f4a219e77faebe6c74b9340
Shubham Singh <shubhamkumar2452004@gmail.com>
2026-07-04 13:15:24 +0530

    Cheack api test for render server

--- backend/routes/judgeMeRoutes.js
+++ backend/routes/judgeMeRoutes.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const judgeMeApi = require("../services/judgeMeService");
 
 router.get("/test", async (req, res) => {
-  res.json({
-    success: true,
-    message: "Judge.me Route Working",
-  });
+  try {
+    const response = await judgeMeApi.get("/reviews", {
+      params: {
+        per_page: 1,
+      },
+    });
+
+    res.json({
+      success: true,
+      total_reviews: response.data.total_count,
+      data: response.data.reviews,
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+
+    res.status(500).json({
+      success: false,
+      error: error.response?.data || error.message,
+    });
+  }
 });
 
 module.exports = router;
